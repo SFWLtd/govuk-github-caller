436233081eac21f0092944a71fd3197921eb6178
David Raine <DavidR@sfwltd.co.uk>
2016-03-29 14:53:28 +0100

    Add select list to view by department

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,14 @@ import * as fs from "fs";
 
 export function onRequest(request: http.IncomingMessage, response: http.ServerResponse): void {
     if (request.url === "/api") {
-        response.writeHead(200, { "content-type": "text/json" });
+        response.writeHead(200, { "content-type": "application/json" });
         
         var users = db.default.getUsers();
         
-        var results = [].concat.apply([], users.map(function(x){
-            return x.languages;
+        var results = [].concat.apply([], users.map(u => {
+            return u.languages.map(l => {
+                return { user: u.name, repository: l.repository, language: l.language };
+            });
         }));
         
         response.write(JSON.stringify(results));
